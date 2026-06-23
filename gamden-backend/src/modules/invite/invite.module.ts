a372e0fb0ec71dbe69e8f4233f4ab0c38ad66679
BiglionX <biglionx@users.noreply.github.com>
2026-06-24 02:39:10 +0800

    fix: register Territory repository in InviteModule for InviteService DI

--- gamden-backend/src/modules/invite/invite.module.ts
+++ gamden-backend/src/modules/invite/invite.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Invite } from '../../entities/invite.entity';
 import { User } from '../../entities/user.entity';
+import { Territory } from '../../entities/territory.entity';
 import { InviteService } from './invite.service';
 import { InviteController } from './invite.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Invite, User])],
+  imports: [TypeOrmModule.forFeature([Invite, User, Territory])],
   providers: [InviteService],
   controllers: [InviteController],
   exports: [InviteService],
